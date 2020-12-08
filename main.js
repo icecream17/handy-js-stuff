b87ccdf6ff4264247926fc25c6d0d35c06d58aab
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-12-08 08:41:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,70 @@
+
+/////////////////////////////////////
+(function(){
+   
+globalThis.getNeatParams = function (func) {
+   let str = func
+   return ""; // UGH not now, todo
+}
+
+})()
+///////////////////////////////////////
+
+
 // Please see https://github.com/icecream17/handy-js-stuff
 // Comment line 11 if you want to redefine any of the functions with external code.
+Object.defineProperty(globalThis, "makeFunctionNative", {
+   enumerable: false,
+   configurable: true,
+   writable: true,
+   value: (function nativeCodeClosure() {
+      function makeFunctionNative(func) {
+         func.toString = (function toString() {return `function ${this.name} ${getNeatParams(func)} { [native code] }`}).bind(func)
+         func.toString.toString = 
+      }
+      return makeFunctionNative(makeFunctionNative);
+   })()
+}
+
 
 Object.defineProperty(globalThis, "defineGlobals", {
-   value:
-   function defineConstantGlobals(allVariables) {
-      for (let [key, value] of Object.entries(allVariables) {
-         allVariables[key] = {
-            "value": value,
-            writable: false,
-            set: function constantGlobalCannotBeChanged(value) {throw "You cannot change this! Hmmph. (is-constant)"}
-         };
+   enumerable: false,
+   configurable: true,
+   writable: true,
+   value: (function nativeCodeClosure() {
+      function defineGlobals(allVariables) {
+         for (let [key, value] of Object.entries(allVariables) {
+            allVariables[key] = {
+               enumerable: false,
+               configurable: true,
+               writable: true,
+               "value": value
+            };
+         }
+
+         Object.defineProperties(globalThis, allVariables);
       }
-      
-      Object.defineProperties(globalThis, allVariables);
-   }
+      defineGlobals.toString = "function defineGlobals { [native code] }";
+      return defineGlobals;
+   })()
 });
 
 Object.defineProperty(globalThis, "isStrict", {
-   get:
-   function isStrict (keys, values) {
+   enumerable: false,
+   configurable: true,
+   writable: true,
+   get: function isStrict (keys, values) {
       try {NaN = 7} catch {return false}
       return true
    }
 });
+
+Object.defineProperty(Object, "shallowCopy", {
+   enumerable: false,
+   configurable: true,
+   writable: true,
+   value: 
+}
 
 globalThis.defineGlobals({
 
@@ -31,21 +73,29 @@ globalThis.defineGlobals({
     *  keys Object  => for..of keys, values = object values
     */
    Enum:
-   function Enum (keys, values, allowUnusualType = false) {
-      function createStartEnum() {
-         return {};
+   function Enum (arg1, arg2) {
+      function createEnumObject() {
+         let enumObject = {};
+         if (arg2 === undefined) {
+            return Object.shallowCopy(arg1);
+         } else {
+            for (let i = 0; i < arg1.length; i++) {
+               enumObject[arg1[i]] = arg2[i]
+            }
+         }
+         return enumObject;
       }
    
       const handler = {
          get(target, name) {
-            if (typeof target[name] !== 'undefined') return target[name];
-            throw new Error(`No such enumerator: ${name}`);
+            if (!(name in target)) throw new ReferenceError(`No such enumerator: ${name}`);
+            return target[name];
          },
          set() {
             throw new Error('Cannot add/update properties on an Enum instance after it is defined')
          }
        };
 
-       return new Proxy(createStartEnum(), handler);
+       return new Proxy(createEnumObject(), handler);
    }
 });

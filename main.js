90c5e0939535daa746c681c85cddaf5ddc68db8f
steven nguyen <58114641+icecream17@users.noreply.github.com>
2021-01-09 17:52:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,101 +1,46 @@
-
-/////////////////////////////////////
-(function(){
-   
-globalThis.getNeatParams = function (func) {
-   let str = func
-   return ""; // UGH not now, todo
+// For pausing code
+async function pause(ms) {
+   return await new Promise(resolve => setTimeout(resolve, ms, "Done!"));
 }
 
-})()
-///////////////////////////////////////
-
-
-// Please see https://github.com/icecream17/handy-js-stuff
-// Comment line 11 if you want to redefine any of the functions with external code.
-Object.defineProperty(globalThis, "makeFunctionNative", {
-   enumerable: false,
-   configurable: true,
-   writable: true,
-   value: (function nativeCodeClosure() {
-      function makeFunctionNative(func) {
-         func.toString = (function toString() {return `function ${this.name} ${getNeatParams(func)} { [native code] }`}).bind(func)
-         func.toString.toString = 
-      }
-      return makeFunctionNative(makeFunctionNative);
-   })()
+// Delete at a specific index
+Array.prototype.delete = function deleteAtIndex (index) {
+   return this.splice(index, 1);
 }
 
+// Add value(s) at a specific index
+Array.prototype.add = function addAtIndex (index, ...things) {
+   return this.splice(index, 0, ...things)
+}
 
-Object.defineProperty(globalThis, "defineGlobals", {
-   enumerable: false,
-   configurable: true,
-   writable: true,
-   value: (function nativeCodeClosure() {
-      function defineGlobals(allVariables) {
-         for (let [key, value] of Object.entries(allVariables) {
-            allVariables[key] = {
-               enumerable: false,
-               configurable: true,
-               writable: true,
-               "value": value
-            };
+// To wait until some condition
+const TICK_SPEED = 50;
+let ticks = 0;
+let todo = [];
+
+todo.prototype.addFunction = function add(func, times) {
+   if (times === undefined) {
+      todo.push(func)
+      return todo.length - 1;
+   } else if (Number.isInteger(times) && times > 0) {
+      todo.push(func)
+      todo.push((amount, length) => {
+         amount--;
+         if (amount === 0) todo.delete(length)
+         else {
+            
          }
-
-         Object.defineProperties(globalThis, allVariables);
-      }
-      defineGlobals.toString = "function defineGlobals { [native code] }";
-      return defineGlobals;
-   })()
-});
-
-Object.defineProperty(globalThis, "isStrict", {
-   enumerable: false,
-   configurable: true,
-   writable: true,
-   get: function isStrict (keys, values) {
-      try {NaN = 7} catch {return false}
-      return true
+      }.bind(null, times, todo.length))
+      return todo.length - 2;
+   } else if (times === 0) {
+      console.warn("...0 times?") 
+   } else {
+      console.error("Invalid amount of times");
    }
-});
-
-Object.defineProperty(Object, "shallowCopy", {
-   enumerable: false,
-   configurable: true,
-   writable: true,
-   value: 
 }
 
-globalThis.defineGlobals({
-
-   /*  keys, values => for..of keys, values
-    *  keys Array   => for..of keys, values = indices
-    *  keys Object  => for..of keys, values = object values
-    */
-   Enum:
-   function Enum (arg1, arg2) {
-      function createEnumObject() {
-         let enumObject = {};
-         if (arg2 === undefined) {
-            return Object.shallowCopy(arg1);
-         } else {
-            for (let i = 0; i < arg1.length; i++) {
-               enumObject[arg1[i]] = arg2[i]
-            }
-         }
-         return enumObject;
-      }
-   
-      const handler = {
-         get(target, name) {
-            if (!(name in target)) throw new ReferenceError(`No such enumerator: ${name}`);
-            return target[name];
-         },
-         set() {
-            throw new Error('Cannot add/update properties on an Enum instance after it is defined')
-         }
-       };
-
-       return new Proxy(createEnumObject(), handler);
-   }
-});
+function RunClockThings () {
+   for (let thing of todo) thing();
+   ticks++;
+}
+let ClockInterval = setInterval(RunClockThings, TICK_SPEED)
